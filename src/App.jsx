847636176e2b2871857b3a87dde3517a16e21453
Sanzial <dj.thaomeo@gmail.com>
2022-07-13 18:16:39 +0700

    Auth - Register and Login

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import "primereact/resources/themes/lara-light-teal/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 
-import { Container } from "react-bootstrap";
-import AdminPage from "./Admin/AdminPage";
+import Login from "./Components/Login";
+import { Fragment } from "react";
+import Register from "./Components/Register";
+// import AdminPage from "./Components/Admin/AdminPage";
 
 function App() {
   return (
-    <Container className="text-center" style={{backgroundColor:"white"}}>
-      <AdminPage/>
-    </Container>
+    <Fragment>
+      {/* <AdminPage /> */}
+      <Login />
+      {/* <Register /> */}
+    </Fragment>
   );
 }
 
